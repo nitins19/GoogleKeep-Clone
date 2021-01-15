a5c2cbd2dbe8f547a4e592500fb9fd9f3ee5cbc5
nitins19 <nitinsaxena913@gmail.com>
2021-01-16 04:09:08 +0530

    first committ

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import CreateNote from "./CreateNote.jsx";
+import Note from "./Note.jsx"
+function App()
+{
+const[addNotes,updatedaddNotes]=useState([]);
+function newNotes(note)
+{
+updatedaddNotes((prevnotes)=>{
+    return[...prevnotes,note];
+});
+
+}
+function deleteNote(id)
+{
+    updatedaddNotes((arr)=>{
+        return arr.filter((arrelements,index)=>{
+            return (index!==id);
+        });
+    });
+}
+    return(<>
+    <Header/>
+    <CreateNote passNotes={newNotes}/>
+   {addNotes.map((val,index)=>{
+        return(
+            <Note
+                key={index}
+                id={index}
+                title={val.title}
+                content={val.content}
+                delete={deleteNote}
+            />
+        );
+    })}
+    <Footer/>
+    </>);
+}
+export default App;
---
+++ src/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Header()
+{
+    return(<>
+    <div className="headingtag">
+    <img src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png" alt="loading"  height='50' style={{marginTop: '8px',marginLeft:'10px'}}></img>
+    <h1 className="heading">NKeep</h1>
+    </div>
+    </>);
+}
+export default Header;
